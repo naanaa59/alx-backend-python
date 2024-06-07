d806d64fe6ce14b3b51f5317e603ff8b4224dd3d
oumaima <oumaimanaanaapro@gmail.com>
2024-06-07 22:03:13 +0100

    2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This script describes a typed annoted function named floor"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """ returns the floor of n """
+    return math.floor(n)
