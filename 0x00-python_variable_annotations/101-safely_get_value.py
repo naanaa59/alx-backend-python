2b6a69788d6824f22c3058bf1705ac3cde545a6b
oumaima <oumaimanaanaapro@gmail.com>
2024-06-08 23:28:23 +0100

    101-

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Type-annotated function usin TypeVar """
+
+
+from typing import Mapping, Any, Union, Optional, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default:
+                     Union[T, None]) -> Union[Any, T]:
+    """ return value of a dictionnary """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
