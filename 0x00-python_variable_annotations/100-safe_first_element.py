9ad80828333bf88bb477082a4852668e2252d9ef
oumaima <oumaimanaanaapro@gmail.com>
2024-06-08 23:02:02 +0100

    doc

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-# The types of the elements of the input are not know
+""" The types of the elements of the input are not know """
 
 from typing import Sequence, Union, Any, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, Optional[Any]]:
-    # safe first element
+    """ safe first element """
     if lst:
         return lst[0]
     else:
